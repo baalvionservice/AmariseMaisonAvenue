f9fe15c763f27fbaf2317ea1aa4e816cc3523a45
Infrastructure Baalvion <infra.baalvion@gmail.com>
2026-03-27 06:41:53 +0000

    Now do according to this

--- src/app/[country]/layout.tsx
+++ src/app/[country]/layout.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import React from 'react';
@@ -48,7 +49,10 @@ export default function CountryLayout({
               <ul className="space-y-4 text-[13px] font-light text-gray-600">
                 <li><Link href={`/${countryCode}/contact`} className="hover:text-black transition-colors">Contact Us</Link></li>
                 <li><Link href={`/${countryCode}/account`} className="hover:text-black transition-colors">My Account</Link></li>
-                <li><Link href={`/${countryCode}/customer-service`} className="hover:text-black transition-colors">Returns & Shipping</Link></li>
+                <li><Link href={`/${countryCode}/customer-service`} className="hover:text-black transition-colors">Shipping & Returns</Link></li>
+                <li><Link href={`/${countryCode}/customer-service`} className="hover:text-black transition-colors">Authenticity Guarantee</Link></li>
+                <li><Link href={`/${countryCode}/how-to-sell`} className="hover:text-black transition-colors">Sell To Us</Link></li>
+                <li><Link href={`/${countryCode}/appointments`} className="hover:text-black transition-colors">Showrooms</Link></li>
               </ul>
             </div>
 
@@ -56,8 +60,12 @@ export default function CountryLayout({
             <div className="space-y-8">
               <h4 className="text-[11px] font-bold tracking-[0.2em] uppercase border-b border-black pb-4 w-full">About</h4>
               <ul className="space-y-4 text-[13px] font-light text-gray-600">
-                <li><Link href={`/${countryCode}/about`} className="hover:text-black transition-colors">Our Heritage</Link></li>
-                <li><Link href={`/${countryCode}/journal`} className="hover:text-black transition-colors">The Journal</Link></li>
+                <li><Link href={`/${countryCode}/about`} className="hover:text-black transition-colors">About Us</Link></li>
+                <li><Link href={`/${countryCode}/contact`} className="hover:text-black transition-colors">Visit Us</Link></li>
+                <li><Link href={`/${countryCode}/about`} className="hover:text-black transition-colors">Message From Our Founder</Link></li>
+                <li><Link href={`/${countryCode}/journal`} className="hover:text-black transition-colors">Affiliates</Link></li>
+                <li><Link href={`/${countryCode}/journal`} className="hover:text-black transition-colors">Blog</Link></li>
+                <li><Link href={`/${countryCode}/journal`} className="hover:text-black transition-colors">Press</Link></li>
               </ul>
             </div>
 
@@ -67,6 +75,9 @@ export default function CountryLayout({
               <ul className="space-y-4 text-[13px] font-light text-gray-600">
                 <li><Link href={`/${countryCode}/category/hermes`} className="hover:text-black transition-colors">Archive Registry</Link></li>
                 <li><Link href={`/${countryCode}/account/live`} className="hover:text-black transition-colors">Live Shops</Link></li>
+                <li><Link href={`/${countryCode}/category/new-arrivals`} className="hover:text-black transition-colors">New Arrivals</Link></li>
+                <li><Link href={`/${countryCode}/category/hermes`} className="hover:text-black transition-colors">Hermès</Link></li>
+                <li><Link href={`/${countryCode}/category/chanel`} className="hover:text-black transition-colors">Chanel</Link></li>
               </ul>
             </div>
 
